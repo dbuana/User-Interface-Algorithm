a36f90d8f6eb49086147ca6c2751b813cfec340d
dbuana <46995714+dbuana@users.noreply.github.com>
2020-07-10 09:47:14 +0900

    Add files via upload
    The code here is different than one, I usually code. Here; I tried to experiment using user-interface and also tried to create algorithm to satisfy the needs of the user-interface. 
    
    If this code could more sophisticated, do not hesitate on creating a pull-request.

---
+++ recurssion.py
@@ -0,0 +1,41 @@
+# Python; Introduction to Data Structures and Algorithms
+import random
+
+# Arranging Array Algorithm
+# Goal: Using "user-input", they have the opportunity on crating their own number arrays and function has to organize the numbers
+data = []
+
+def userInput():
+	# The user input is heliocentric to this code
+	n1 = int(input(""))
+	n2 = int(input(""))
+	n3 = int(input(""))
+	n4 = int(input(""))
+	# Calling the interface
+	print(n1)
+	print(n2)
+	print(n3)
+	print(n4)
+	# Implementing the interface 
+	data = [n1, n2, n3, n4]
+	return dataI
+
+def organize_data():
+	data = [n1, n2, n3, n4]
+
+	for i in dataI:
+		if n[int(-1)] > n[int(1)]:
+			print(n)
+	return n
+
+print(userInput())
+print(organize_data())
+
+# The algorithm is considerably simple, in terms of concept. The concept is a user creates their
+# own number array and then the program tries to arrange the numbers. 
+
+# Inside/Analyzing: I have included the user-interface by simply using the input method, in addition
+# I added a "for loop". For the purpose of validating each numbers present in the array. 
+# With that; the result should be the numbers listed, in order. 
+
+# Challenge 2; Linear Algorithm
